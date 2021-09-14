525f1a8edea6521c250938ac46eb3517a2d8f639
ConfuzzedCat <confuzzedcat@gmail.com>
2021-09-14 17:12:05 +0200

    Test mode and other stuff

--- NyaBackground/ImageDownloader.cs
+++ NyaBackground/ImageDownloader.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NekosSharp;
 
 namespace NyaBackground
 {
     class ImageDownloader
     {
+        public static NekoClient imgDownloader = new NekoClient("NyaBackground");
+        
+        public static async Task NekoAsync()
+        {
+
+            imgDownloader.SendRequest();
+            Console.WriteLine("Requesting image...");
+            Request Req = await imgDownloader.Image_v3.Neko(1);
+            Console.WriteLine(Req.ImageUrl);
+            Console.WriteLine("Image downloaded");
+            //Console.WriteLine(Req.Error);
+            //imgDownloader.Image.Neko();
+            
+        }
+
     }
 }
