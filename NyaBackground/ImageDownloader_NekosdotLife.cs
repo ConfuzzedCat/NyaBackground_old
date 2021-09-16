7ecdee6716e22ef202190a6bf9b6e4d8300526b0
ConfuzzedCat <confuzzedcat@gmail.com>
2021-09-16 20:22:23 +0200

    removed some spaces

--- NyaBackground/ImageDownloader_NekosdotLife.cs
+++ NyaBackground/ImageDownloader_NekosdotLife.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.IO;
-using System.Net;
-using Newtonsoft.Json.Linq;
-
+﻿using System.Net;
 namespace NyaBackground
 {
     class ImageDownloader_NekosdotLife : ImageDownloader
